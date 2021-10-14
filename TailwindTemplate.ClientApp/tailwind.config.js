d59e0b4400971573a241dd6b4f23d6485b49c2b8
Jon Hilton <hilton.jon@gmail.com>
2021-10-14 12:46:32 +0100

    Initial Commit

---
+++ TailwindTemplate.ClientApp/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: [
+        './**/*.razor',
+        './**/*.cshtml'
+    ]
+},
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
