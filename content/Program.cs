e4e2987902441bb744bb32323f256a3c22a07018
Jon Hilton <hilton.jon@gmail.com>
2021-10-20 11:31:06 +0100

    Updated everything to .NET 6.

--- content/Program.cs
+++ content/Program.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Net.Http;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using TailwindTemplate.ClientApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
