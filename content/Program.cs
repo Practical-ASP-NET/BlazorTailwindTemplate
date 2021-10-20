5cfd048d1e84c527c6b3599143b831cad0b53e67
Jon Hilton <hilton.jon@gmail.com>
2021-10-20 08:10:17 +0100

    Configured .NET 5 version of the project (not yet sure what the best way is to support different framework versions)

--- content/Program.cs
+++ content/Program.cs
@@ -1,10 +1,11 @@
-using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using TailwindTemplate.ClientApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
